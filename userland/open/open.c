ce853323452adf222382cf1648a7c8da66d666e4
root <root@localhost.(none)>
2018-12-01 07:09:28 -0500

    create open api

---
+++ userland/open/open.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "open.h"
+
+int main(int argc, char **argv)
+{
+	printf("hello word.\n");
+
+	return 0;
+}
---
+++ userland/open/open.h
@@ -0,0 +1,4 @@
+#ifndef __open_h__
+#define __OPEN_H__
+
+#endif
